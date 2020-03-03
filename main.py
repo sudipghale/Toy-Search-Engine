32362b7a7e23dff6b8b06c736624aec4f1c2db56
sudip ghale <alliswell.sudip@gmail.com>
2020-03-02 18:59:24 -0600

    main upaded

--- main.py
+++ main.py
@@ -9,29 +9,25 @@ from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
-tf_token = []
-df_token = []
-corpusroot = './presidential_debates'
+tf_token = {}
+df_token = Counter()
+stemmed_words =[]
+
+corpusroot = './test'
 tokenizer = RegexpTokenizer(r'[a-zA-Z]+') 
 stemmer = PorterStemmer()
-
+stop_words_eng = stopwords.words("english")
 
 for filename in os.listdir(corpusroot):
     file = open(os.path.join(corpusroot, filename), "r", encoding='UTF-8')
     doc = file.read()
     file.close() 
     doc = doc.lower()
+    tokens = tokenizer.tokenize(doc)
+    tokens = [stemmer.stem(word) for word in tokens if word not in stop_words_eng]
+    tf_tok = Counter(tokens)
+    tf_token[filename] = tf_tok.copy()
+    df_token = df_token + Counter(set(tokens))
 
-
-
-tokens = tokenizer.tokenize(doc)
-filtered_words = [word for word in tokens if word not in stopwords.words('english')]
-
-print(len(filtered_words))
-
-stemmed_words =[]
-for word in filtered_words:
-    stemmed_words.append(stemmer.stem(word))
-
-print(len(stemmed_words))
-
+    
+    
