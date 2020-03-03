809b2cd25a698c0baf53f969e2a3894a6e953437
sudip ghale <alliswell.sudip@gmail.com>
2020-03-02 23:21:56 -0600

    fn getidf and getweight added seems to be working

--- main.py
+++ main.py
@@ -29,5 +29,43 @@ for filename in os.listdir(corpusroot):
     tf_token[filename] = tf_tok.copy()
     df_token = df_token + Counter(set(tokens))
 
+print(tf_token)
+print(df_token)    
+
+#* getidf(token): return the inverse document frequency of a token. If the token doesn't exist in the corpus, return -1. 
+# The parameter 'token' is already stemmed. (It means you should not perform stemming inside this function.) Note the 
+# differences between getidf("hispan") and getidf("hispanic"). 
+
+def getidf(token):
+    n = len(tf_token)
+    print("N is =", n)
+    if df_token[token] == 0:
+        return -1
+    else:
+        return math.log10(n/df_token[token])  
+    
+#* getweight(filename,token): return the TF-IDF weight of a token in the document named 'filename'. 
+# If the token doesn't exist in the document, return 0. The parameter 'token' is already stemmed. 
+# (It means you should not perform stemming inside this function.) Note that both getweight("1960-10-21.txt","reason") 
+# and getweight("2012-10-16.txt","hispanic") return 0, but for different reasons. 
+
+def getweight(filename, token): #tf-idf weight
+    idf_token = getidf(token)
+    print("idf of token",idf_token)
+    if any(token in word for word in tf_token[filename]):
+        tf_idf_token =(1+math.log10(tf_token[filename][token]))* idf_token
+        print("token found in doc with tf-idf score", tf_idf_token)
+        return tf_idf_token
+    else:
+        return 0
+        
+        
+#*query(qstring): return a tuple in the form of (filename of the document, score), where the document is the query answer with respect 
+# to "qstring" . If no document contains any token in the query, return ("None",0). If we need more than 10 elements from each posting list,
+#  return ("fetch more",0).
+        
     
     
+
+tf_idf = getweight("file1.txt", "octob")
+print("tf-idf", tf_idf);
